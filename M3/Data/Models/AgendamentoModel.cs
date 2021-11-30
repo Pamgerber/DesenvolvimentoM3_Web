af1af69d87fe4e2af167e866aca67a08e964b5dc
vinicius-vs <vinicius_stumpf@edu.univali.br>
2021-11-29 22:41:33 -0300

    Criando o base model e agendamento model

---
+++ M3/Data/Models/AgendamentoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class AgendamentoModel:BaseModel
+    {
+        public string motorista { get; set; }
+        public string placa { get; set; }
+        public decimal pesoMax { get; set; }
+        public DateTime diaColeta { get; set; }
+    }
+}
