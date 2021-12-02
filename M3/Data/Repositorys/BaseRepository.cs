ede1c1627e5919ab815371a62f1415c134c27ced
vinicius-vs <vinicius_stumpf@edu.univali.br>
2021-12-01 22:44:38 -0300

    Criado o repositorio base :3

--- M3/Data/Repositorys/BaseRepository.cs
+++ M3/Data/Repositorys/BaseRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Data.Context;
+using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,49 @@ using System.Threading.Tasks;
 
 namespace Data.Repositorys
 {
-    class BaseRepository
+    class BaseRepository<T> where T : BaseModel
     {
+        public void Create(T model)
+        {
+            using (var context = new LabContext())
+            {
+                context.Set<T>().Add(model);
+                context.SaveChanges();
+            }
+        }
+        public List<T> Read()
+        {
+            List<T> lista = new List<T>();
+            using (var context = new LabContext())
+            {
+                lista = context.Set<T>().ToList();
+            }
+            return lista;
+        }
+        public T Read(int id)
+        {
+            T model = Activator.CreateInstance<T>();
+            using (var context = new LabContext())
+            {
+                model = context.Set<T>().Find(id);
+            }
+            return model;
+        }
+        public void Update(T model)
+        {
+            using (var context = new LabContext())
+            {
+                context.Entry<T>(model).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void Delete(int id)
+        {
+            using (var context = new LabContext())
+            {
+                context.Entry<T>(Read(id)).State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
     }
 }
