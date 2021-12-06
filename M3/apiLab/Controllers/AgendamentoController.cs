110d52b6ca04c63fa731574747dc725d2fbd0e0c
vinicius-vs <vinicius_stumpf@edu.univali.br>
2021-12-06 20:56:08 -0300

    criado a api e o krl a

---
+++ M3/apiLab/Controllers/AgendamentoController.cs
@@ -0,0 +1,16 @@
+﻿using Data.Models;
+using Data.Repositorys;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiLab.Controllers
+{
+    public class AgendamentoController : BaseController<AgendamentoRepository,AgendamentoModel>
+    {
+
+    }
+}
